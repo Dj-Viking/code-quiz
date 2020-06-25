217f49cd379cbf201f99ebfbe6079449f3bc0f18
DJ Viking <anders.swedishviking@gmail.com>
2020-06-25 16:32:47 -0400

    created new page for the quiz. kept index.html as the main page to start the game

---
+++ assets/js/logic.js
@@ -0,0 +1,17 @@
+// must dynamically generate and append elements that have text in them 
+//  into their respective parent elements
+
+//have a starting page with only one button with start game written on it
+//this button will trigger to link into another html document identical
+//  but has the answer buttons updated into the quiz box
+
+//feedback at the bottom of the answer buttons will change according to
+//  the answer that matches a correct choice
+//  regardless of what answer they give will start dynamically generating the next
+//  question and set of answer buttons
+
+//the first page will have the timer but wont begin until the start game button is pushed
+
+//view high scores will link to a separate html page with the scores display
+//then have a link to go back to start game page
+

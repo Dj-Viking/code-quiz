3f2dd451f3629b53bc4e21866ce8583f326730b9
DJ Viking <anders.swedishviking@gmail.com>
2020-06-28 00:46:23 -0400

    able to display page 2 question and answers. now on to page 3

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -25,11 +25,18 @@ var goToQuestions = document.querySelector("#text-link");
 var questionBox = document.querySelector("#quiz-question-text");
 var startGameText = document.querySelector("#start-game-text");
 var startButton = document.querySelector("#start-button-area");
+
 var answerButtons = document.querySelector("#answer-buttons-area");
 var answerButton1 = document.querySelector("#submit-answer1");
 var answerButton2 = document.querySelector("#submit-answer2");
 var answerButton3 = document.querySelector("#submit-answer3");
 var answerButton4 = document.querySelector("#submit-answer4");
+
+
+
+var startpageAnswerInvisibleToVisible = document.querySelector(".answer-button")
+
+
 
 
 function createQuestions(){
@@ -43,8 +50,15 @@ function createQuestions(){
 }
 
 function createAnswerButtonsPage1(){
+    //remove start button
     startButton.setAttribute("style", "display: none;");
-    answerButtons.setAttribute("style", "display: block;");
+
+    //make answer buttons appear
+    startpageAnswerInvisibleToVisible.setAttribute("style", "display: block;");
+    
+    answerButtons.setAttribute("style", "display: flex;");
+    answerButtons.setAttribute("style", "visibility: visible;");
+
     
     //make the containers appear
     answerButton1.setAttribute("style", "display: block;");
@@ -53,16 +67,16 @@ function createAnswerButtonsPage1(){
     answerButton4.setAttribute("style", "display: block;");
     
     //create divs to append to the container
-    var answerButton1Div = document.createElement("div")
-    var answerButton2Div = document.createElement("div")
-    var answerButton3Div = document.createElement("div")
-    var answerButton4Div = document.createElement("div")
+    var answerButton1Div = document.createElement("div");
+    var answerButton2Div = document.createElement("div");
+    var answerButton3Div = document.createElement("div");
+    var answerButton4Div = document.createElement("div");
 
     //assigning classnames to the divs
-    answerButton1Div.className = "answer-button-text";
-    answerButton2Div.className = "answer-button-text";
-    answerButton3Div.className = "answer-button-text";
-    answerButton4Div.className = "answer-button-text";
+    answerButton1Div.id = "answer-button-text1";
+    answerButton2Div.id = "answer-button-text2";
+    answerButton3Div.id = "answer-button-text3";
+    answerButton4Div.id = "answer-button-text4";
 
     //writing the text to the div 
     answerButton1Div.textContent = answerButtonArray1[0];
@@ -79,11 +93,70 @@ function createAnswerButtonsPage1(){
 }
 
 function createAnswerButtonsPage2(){
+    //this will handle once one of the answers is clicked all the question buttons will
+
     
+    //hide the first set of divs with page 1 questions
+    var answerButton1Div = document.querySelector("#answer-button-text1")
+    var answerButton2Div = document.querySelector("#answer-button-text2")
+    var answerButton3Div = document.querySelector("#answer-button-text3")
+    var answerButton4Div = document.querySelector("#answer-button-text4")
+    
+    answerButton1Div.setAttribute("style", "display: none;");
+    answerButton2Div.setAttribute("style", "display: none;");
+    answerButton3Div.setAttribute("style", "display: none;");
+    answerButton4Div.setAttribute("style", "display: none;");
+    
+    //create new divs to append to the container
+    var answer2Button1Div = document.createElement("div");
+    var answer2Button2Div = document.createElement("div");
+    var answer2Button3Div = document.createElement("div");
+    var answer2Button4Div = document.createElement("div");
+    
+    //assigning classnames to the divs
+    answer2Button1Div.id = "#answer-button-text4";
+    answer2Button2Div.id = "#answer-button-text5";
+    answer2Button3Div.id = "#answer-button-text6";
+    answer2Button4Div.id = "#answer-button-text7";
+
+    answer2Button1Div.textContent = answerButtonArray2[0];
+    answer2Button2Div.textContent = answerButtonArray2[1];
+    answer2Button3Div.textContent = answerButtonArray2[2];
+    answer2Button4Div.textContent = answerButtonArray2[3];
+
+    answerButton1.appendChild(answer2Button1Div);
+    answerButton2.appendChild(answer2Button2Div);
+    answerButton3.appendChild(answer2Button3Div);
+    answerButton4.appendChild(answer2Button4Div);
+    
+    //repopulate from the questionButtonArray 2
+    var questionText = document.querySelector("#quiz-question-text");
+
+
+    //update the question for page 2
+
+    questionText.textContent = questionArray[1];
 }
+
+
+
 
 goToQuestions.addEventListener("click", createQuestions);
 goToQuestions.addEventListener("click", createAnswerButtonsPage1);
+
+//go to page 2
+answerButton1.addEventListener("click", createAnswerButtonsPage2); 
+answerButton2.addEventListener("click", createAnswerButtonsPage2); 
+answerButton3.addEventListener("click", createAnswerButtonsPage2); 
+answerButton4.addEventListener("click", createAnswerButtonsPage2);
+
+//go to page 3
+
+//go to page 4
+
+//go to high scores
+
+
 
 console.dir(document);
 
